beb93a8391aec1bc63e45416568e08e6293a76ff
Lucas Guerra <67044239+vlucasx@users.noreply.github.com>
2023-07-11 22:04:50 -0300

    teste função de ativação 1

--- neuronio.py
+++ neuronio.py
@@ -1,3 +1,5 @@
+import math
+
 entradas = [3,0.2,0.6] # vetor de entrada 
 pesos = [0.4,0.5,0.5] # vetor de pesos
 limiarAtivacao = 1 # threshold (posteriormentte uma função)
@@ -11,6 +13,8 @@ def verificaEntradasEpesos(entradas, pesos): # verifica se a quantidade de entra
     return
   return True
 
+def funcaoAtivacao(net, deslocamento, temperatura):
+  return 1/(1+(math.e)**(-((net-deslocamento)/temperatura)))
 
 def neuronio(entradas, pesos, limiarAtivacao): # método do neurônio
   net = 0
@@ -35,8 +39,14 @@ def neuronio(entradas, pesos, limiarAtivacao): # método do neurônio
 def main():
   if not verificaEntradasEpesos(entradas, pesos):
     return
-  
   neuronio(entradas, pesos, limiarAtivacao)
+  deslocamento = 0
+  temperatura =-1
 
+  for net in range(0,30):
+    if net/10 > funcaoAtivacao(net/10,deslocamento,temperatura):
+      print("ativado ", "f("+str(net/10)+") = "+str(funcaoAtivacao(net/10,deslocamento,temperatura)))
+    else:
+        print("f("+str(net/10)+") = "+str(funcaoAtivacao(net/10,deslocamento,temperatura)))
 
 main()
